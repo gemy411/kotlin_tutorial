ffcc3847d76385c573d2ccd6683e899058d7008f
mohamed gamal <mohamed11gamal96@gmail.com>
2018-12-09 17:08:03 +0200

    looping

--- src/Kotlin.kt
+++ src/Kotlin.kt
@@ -1,21 +1,37 @@
+import java.util.Random
 fun main(args: Array<String>) {
 
-
-    val age = 8
-    if (age < 5) {
-        println("ok")
-    }else {
-        println("shit")
+    for (x in 1..100) {
+        println("Loop : $x")
     }
-    when (age) {
-        0, 1, 2, 3, 4 -> println("when age")
-        5 -> println("hsit")
-        in 6..17 -> {
-            val grade = age - 5
-            println("go to grade $grade")
+    val rand = Random()
+    val magicNum = rand.nextInt(50) + 1
+    var guess = 0
+    while (magicNum != guess) {
+        guess += 1
+
+    }
+    println("Magic Number was $guess")
+    for (x in 1..200) {
+        if (x % 2 == 0) {
+            continue
         }
-        else -> println("go home")
+        println("Odd : $x")
+        if (x == 15 ) break
+    }
+    var arr3: Array<Int> = arrayOf(3, 6, 9, 7, 5, 4, 3, 5, 7, 8, 87)
 
+
+    for (i in arr3.indices) {
+        println(arr3[i])
+    }
+    for ((index, value) in arr3.withIndex()) {
+        println("Index : $index Value : $value")
 
     }
+
+
+
+
 }
+
