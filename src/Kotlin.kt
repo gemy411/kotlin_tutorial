9ebda69cf115161b9f4800cdb4b74d9ebcd39d73
mohamed gamal <mohamed11gamal96@gmail.com>
2018-12-10 13:13:27 +0200

    Lists

--- src/Kotlin.kt
+++ src/Kotlin.kt
@@ -2,15 +2,26 @@ import java.lang.IllegalArgumentException
 
 fun main(args: Array<String>) {
 
-    val divisor = 0
-    try {
-        if (divisor == 0) {
-            throw IllegalArgumentException("Can't divide by zero")
+    var list1: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5)
+    val list2: List<Int> = listOf(1, 2, 3, 4, 5)
 
-        } else {
-            println("5 / $divisor = ${5 / divisor}")
-        }
-    } catch (e: IllegalArgumentException) {
-        println("${e.message}")
-    }
+    list1.add(6)
+    println("1st : ${list1.first()}")
+    println("last: ${list1.last()}")
+
+    println("2nd : ${list1[2]}")
+
+    var list3 = list1.subList(0, 3)
+
+    println("length: ${list1.size}")
+
+    list3.clear()
+    list1.removeAt(1)
+    list1.forEach({n -> println("Multable List : $n")})
+
+
+
+
+
+
 }
